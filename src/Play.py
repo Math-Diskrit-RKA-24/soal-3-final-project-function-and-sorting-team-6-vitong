23b278d7c8e1daa2665af04a6c3ccfcafcef2285
Arvito Rajapandya Natlysandro <120700269+ExpensiveDrPhill@users.noreply.github.com>
2024-12-04 09:04:03 +0700

    Update Play.py

--- src/Play.py
+++ src/Play.py
@@ -3,7 +3,7 @@ import game as g
 g.initPlayers()
 
 g.addPlayer(g.createNewPlayer('Ajip', damage=20, defensePower=10))
-g.addPlayer(g.createNewPlayer('Vito', damage=0, defensePower=20))
+g.addPlayer(g.createNewPlayer('Vito', damage=100, defensePower=20))
 g.addPlayer(g.createNewPlayer('Henry', damage=10, defensePower=5))
 print(g.PlayerList)
 
